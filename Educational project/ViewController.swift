a89113db14f19ae8749f9bb2b93998dd2b8286e6
Nikita Z <Typenekrasov@gmail.com>
2025-01-29 21:03:42 +0200

    Initial Commit

---
+++ Educational project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Educational project
+//
+//  Created by Никита  on 29.1.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Educational projectTests/Educational_projectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Educational_projectTests.swift
+//  Educational projectTests
+//
+//  Created by Никита  on 29.1.2025.
+//
+
+import Testing
+@testable import Educational_project
+
+struct Educational_projectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
