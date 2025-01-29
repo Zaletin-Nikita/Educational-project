606bc62ebfea167b0b38104c18a0bbca6d849419
Nikita Z <Typenekrasov@gmail.com>
2025-01-29 22:08:50 +0200

    first commit

--- Educational project/ViewController.swift
+++ Educational project/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+
+        let label = UILabel()
+        label.text = "DeFiHub"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .black
+        label.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
+        view.addSubview(label)
+
+        let imageView = UIImageView()
+        imageView.frame = CGRect(x: 50, y: 220, width: 300, height: 300)
+        imageView.image = UIImage(named: "cosmos-atom-logo")
+        imageView.contentMode = .scaleAspectFit
+        view.addSubview(imageView);
+
     }
-
-
 }
-
