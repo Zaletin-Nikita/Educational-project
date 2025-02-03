51c2371fe7207f1462fee968331cc9fb79a54d74
Nikita Z <Typenekrasov@gmail.com>
2025-02-03 16:19:42 +0200

    Your commit message here

--- Educational project/SplashViewController.swift
+++ Educational project/SplashViewController.swift
@@ -10,31 +10,7 @@ import UIKit
 class SplashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .white // Фон экрана
-        showLogo()
-    }
-
-    func showLogo() {
-        let logo = UIImageView(image: UIImage(named: "logo"))
-        logo.frame = CGRect(x: 100, y: 300, width: 200, height: 200)
-        logo.image = UIImage(named: "cosmos-atom-logo")
-        logo.contentMode = .scaleAspectFit
-        view.addSubview(logo)
         
-    
-        // Анимация исчезновения и переход на главный экран
-        UIView.animate(withDuration: 1.5, delay: 2, options: .curveEaseOut, animations: {
-            logo.alpha = 0
-        }) { _ in
-            self.goToMainScreen()
-        }
-    }
-
-    func goToMainScreen() {
-        let mainVC = ViewController() // Заменить на твой главный экран
-        mainVC.modalTransitionStyle = .crossDissolve
-        mainVC.modalPresentationStyle = .fullScreen
-        present(mainVC, animated: true)
+        
     }
 }
--- Educational project/ViewController.swift
+++ Educational project/ViewController.swift
@@ -10,16 +10,7 @@ import UIKit
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
-
-        let label = UILabel()
-        label.text = "DeFiHub"
-        label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
-        label.textColor = .black
-        label.frame = CGRect(x: 100, y: 100, width: 200, height: 50)
-        view.addSubview(label)
-
+    
 
     }
 }
