556d8da01a08a685a2f5ca967d3b15b23a09aac3
Nikita Z <Typenekrasov@gmail.com>
2025-02-04 22:42:49 +0200

    Fix: Added missing changes to SplashViewController

--- Educational project/SplashViewController.swift
+++ Educational project/SplashViewController.swift
@@ -10,7 +10,34 @@ import UIKit
 class SplashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        setupUI()
+    }
+    
+    private func setupUI() {
+        var label = UILabel()
+        label.text = "CryptoLab"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 24, weight: .black)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
         
+        var logo = UIImageView(image: UIImage(named: "cosmos-atom-logo.svg"))
+        logo.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(logo)
+        
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            logo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logo.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            logo.widthAnchor.constraint(equalToConstant: 200),
+            logo.heightAnchor.constraint(equalToConstant: 200)
+        ])
         
     }
+    
 }
--- Educational project/ViewController.swift
+++ Educational project/ViewController.swift
@@ -7,10 +7,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        view.backgroundColor = .systemBlue
 
+        let label = UILabel()
+        label.text = "Главный экран"
+        label.textColor = .white
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(label)
+
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 }
