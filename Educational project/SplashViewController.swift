ff85daa3797c65c9320c8e39f18e417d895df2a7
Nikita Z <Typenekrasov@gmail.com>
2025-02-05 22:31:40 +0200

    Добавил  gitignore .Вынес все конфигурации  в один метод.Лайбл и лого сделал приватными и вынес в глобальном поле класса.

--- Educational project/SplashViewController.swift
+++ Educational project/SplashViewController.swift
@@ -7,32 +7,44 @@
 
 import UIKit
 
+
 class SplashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        initializeUIComponents()
         setupUI()
     }
-    var label = UILabel()
-    var logo = UIImageView(image: UIImage(named: "cosmos-atom-logo.svg"))
     
-    private func setupUI() {
+    private var label = UILabel()
+    private var logo = UIImageView()
+    
+    private func initializeUIComponents(){
+        // Лейбл
         
         label.text = "CryptoLab"
         label.textColor = .black
         label.font = UIFont.systemFont(ofSize: 24, weight: .black)
         label.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(label)
+ 
         
-
+        //Логотип
+        
+        logo.image = UIImage(named: "osmosis-osmo-logo.png")
         logo.translatesAutoresizingMaskIntoConstraints = false
+
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        // Доболение элементов
+        view.addSubview(label)
         view.addSubview(logo)
         
+        // Констрейты 
         NSLayoutConstraint.activate([
             label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 60),
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
-        
         NSLayoutConstraint.activate([
             logo.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logo.centerYAnchor.constraint(equalTo: view.centerYAnchor),
--- Educational project/ViewController.swift
+++ Educational project/ViewController.swift
@@ -11,17 +11,6 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBlue
-
-        let label = UILabel()
-        label.text = "Главный экран"
-        label.textColor = .white
-        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(label)
-
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
     }
+    
 }
